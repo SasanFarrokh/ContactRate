6eeeee927328f6242c51fe5a420cbe1899f9727e
Sasan Farrokh <sasan.farrokh@gmail.com>
2016-08-21 13:32:50 +0430

    search focus changes

--- java/ir/cdesign/contactrate/AllFragment.java
+++ java/ir/cdesign/contactrate/AllFragment.java
@@ -1,13 +1,18 @@
 package ir.cdesign.contactrate;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 import ir.cdesign.contactrate.adapters.AllAdapter;
 import ir.cdesign.contactrate.models.AllModel;
@@ -18,6 +23,7 @@ import ir.cdesign.contactrate.models.AllModel;
 public class AllFragment extends Fragment {
 
     RecyclerView recyclerView;
+    TextView search;
     AllModel model = new AllModel();
 
     @Nullable
@@ -25,6 +31,16 @@ public class AllFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.all_tab_layout,container , false);
 
+        search = (TextView) view.findViewById(R.id.search);
+        search.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(search.getWindowToken(), 0);
+                }
+            }
+        }); 
         setRecyclerView(view);
 
         return view;
@@ -35,6 +51,14 @@ public class AllFragment extends Fragment {
         AllAdapter adapter = new AllAdapter(getActivity());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setFocusable(true);
+        recyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.requestFocusFromTouch();
+                return false;
+            }
+        });
     }
 
 }
