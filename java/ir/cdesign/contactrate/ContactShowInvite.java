3b07e953a53fbc9de4dd3dc15417236041cdf45b
Amin Ahrabian <aahrabian@gmail.com>
2016-08-22 23:50:33 +0430

    That Fucking Layout is Done & I am Commenting For You AssHole

--- java/ir/cdesign/contactrate/ContactShowInvite.java
+++ java/ir/cdesign/contactrate/ContactShowInvite.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class ContactShowInvite extends AppCompatActivity {
 
@@ -27,6 +28,17 @@ public class ContactShowInvite extends AppCompatActivity {
             actionBar.setDisplayShowTitleEnabled(false);
             actionBar.setBackgroundDrawable(null);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                ContactShowInvite.this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
