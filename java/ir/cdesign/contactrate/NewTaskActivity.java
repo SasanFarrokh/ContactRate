381388a4b90b4068fa62b3b11fff42c26f3744e1
Amin Ahrabian <aahrabian@gmail.com>
2016-08-23 14:38:36 +0430

    only one fucking layout left

--- java/ir/cdesign/contactrate/NewTaskActivity.java
+++ java/ir/cdesign/contactrate/NewTaskActivity.java
@@ -1,13 +1,49 @@
 package ir.cdesign.contactrate;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import ir.cdesign.contactrate.adapters.TaskAdapter;
+import ir.cdesign.contactrate.models.TaskModel;
 
 public class NewTaskActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+
+        setRecyclerView();
+        setToolbar();
+
     }
+
+    private void setRecyclerView(){
+        recyclerView = (RecyclerView) findViewById(R.id.tasks_recycler);
+        TaskAdapter taskAdapter = new TaskAdapter(NewTaskActivity.this, TaskModel.getData());
+        GridLayoutManager glm = new GridLayoutManager(NewTaskActivity.this,3);
+        recyclerView.setLayoutManager(glm);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(taskAdapter);
+    }
+
+    private void setToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setBackgroundDrawable(null);
+        }
+    }
+
 }
--- java/ir/cdesign/contactrate/adapters/TaskAdapter.java
+++ java/ir/cdesign/contactrate/adapters/TaskAdapter.java
@@ -1,31 +1,73 @@
 package ir.cdesign.contactrate.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import ir.cdesign.contactrate.R;
+import ir.cdesign.contactrate.models.TaskModel;
 
 /**
  * Created by amin pc on 23/08/2016.
  */
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskHolder> {
+
+    private ArrayList<TaskModel> taskModels;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+
+    public TaskAdapter(Context context, ArrayList<TaskModel> task) {
+        this.taskModels = task;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
     @Override
     public TaskHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = layoutInflater.inflate(R.layout.task_layout, parent, false);
+        TaskHolder holder = new TaskHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(TaskHolder holder, int position) {
-
+        TaskModel current = taskModels.get(position);
+        holder.setData(current, position);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return taskModels.size();
     }
 
     public class TaskHolder extends RecyclerView.ViewHolder {
+        TextView Title;
+        CircleImageView imageView;
+        TaskModel current;
+        int position;
+
         public TaskHolder(View itemView) {
             super(itemView);
+            Title = (TextView) itemView.findViewById(R.id.task_title);
+            imageView = (CircleImageView) itemView.findViewById(R.id.task_img);
+        }
+
+
+        public void setData(TaskModel current, int position) {
+
+            this.current = current;
+            this.position = position;
+            this.imageView.setImageResource(current.getImageId());
+            this.Title.setText(current.getTitle());
+
         }
     }
 }
