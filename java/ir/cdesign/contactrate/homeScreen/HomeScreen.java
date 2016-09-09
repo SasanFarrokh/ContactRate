9db1e0394678e34326d311a1b0feb67e99381859
Amin Ahrabian <aahrabian@gmail.com>
2016-09-09 23:33:14 +0430

    d

--- java/ir/cdesign/contactrate/homeScreen/HomeScreen.java
+++ java/ir/cdesign/contactrate/homeScreen/HomeScreen.java
@@ -30,7 +30,6 @@ import ir.cdesign.contactrate.imagePicker.DefaultCallback;
 import ir.cdesign.contactrate.imagePicker.EasyImage;
 import ir.cdesign.contactrate.utilities.AsyncGetNews;
 import ir.cdesign.contactrate.utilities.CalendarTool;
-import ir.cdesign.contactrate.utilities.WallpaperBoy;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -47,7 +46,7 @@ public class HomeScreen extends AppCompatActivity {
 
     GraphPage graphPage = new GraphPage();
     AllRankInv allRankInv = new AllRankInv();
-    UserTab userTab = new UserTab();
+    ir.cdesign.contactrate.homeScreen.userTab userTab = new userTab();
 
     Bitmap profileImage;
 
