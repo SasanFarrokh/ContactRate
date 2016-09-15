5b92355f91222fe744e9313388e968607b3526f0
Amin Ahrabian <aahrabian@gmail.com>
2016-09-15 12:11:57 +0430

    working on Lesson

---
+++ java/ir/cdesign/contactrate/adapters/LessonAllAdapter.java
@@ -0,0 +1,7 @@
+package ir.cdesign.contactrate.adapters;
+
+/**
+ * Created by amin pc on 15/09/2016.
+ */
+public class LessonAllAdapter {
+}
--- java/ir/cdesign/contactrate/lessons/Lesson.java
+++ java/ir/cdesign/contactrate/lessons/Lesson.java
@@ -2,14 +2,51 @@ package ir.cdesign.contactrate.lessons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import ir.cdesign.contactrate.R;
 
-public class Lesson extends AppCompatActivity {
+public class Lesson extends AppCompatActivity implements View.OnClickListener{
+
+    Button all ,mark ;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
+
+        init();
+    }
+
+    private void init(){
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        all = (Button) findViewById(R.id.all_btn);
+        mark = (Button) findViewById(R.id.mark_btn);
+    }
+
+    private void adapterSwitch(){
+
+        all.setOnClickListener(this);
+        mark.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.all_btn: {
+
+                break;
+            }
+            case R.id.mark_btn: {
+
+                break;
+            }
+        }
     }
 }
