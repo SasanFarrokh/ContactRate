63ed16e58c497a0d54d56f70ab81aec0a0b14116
Amin Ahrabian <aahrabian@gmail.com>
2016-09-15 13:44:35 +0430

    rowns and shit

--- java/ir/cdesign/contactrate/lessons/ReadLessonActivity.java
+++ java/ir/cdesign/contactrate/lessons/ReadLessonActivity.java
@@ -3,6 +3,8 @@ package ir.cdesign.contactrate.lessons;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ir.cdesign.contactrate.R;
+
 public class ReadLessonActivity extends AppCompatActivity {
 
     @Override
--- java/ir/cdesign/contactrate/models/LessonModel.java
+++ java/ir/cdesign/contactrate/models/LessonModel.java
@@ -4,5 +4,4 @@ package ir.cdesign.contactrate.models;
  * Created by amin pc on 15/09/2016.
  */
 public class LessonModel {
-
 }
