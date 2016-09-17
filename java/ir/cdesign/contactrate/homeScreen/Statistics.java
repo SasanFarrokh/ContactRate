ed484914434fabe594d8c9dd296aac05b0ab8cd2
Amin Ahrabian <aahrabian@gmail.com>
2016-09-17 12:58:50 +0430

    chart library

--- java/ir/cdesign/contactrate/homeScreen/Statistics.java
+++ java/ir/cdesign/contactrate/homeScreen/Statistics.java
@@ -1,9 +1,18 @@
 package ir.cdesign.contactrate.homeScreen;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ir.cdesign.contactrate.R;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.Chart;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class Statistics extends AppCompatActivity {
 
@@ -11,5 +20,7 @@ public class Statistics extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+
     }
 }
