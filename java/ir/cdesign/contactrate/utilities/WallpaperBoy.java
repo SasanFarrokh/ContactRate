d2c52c9851c2de97f18c3c9df1099f633ad0328c
Amin Ahrabian <aahrabian@gmail.com>
2016-09-17 15:46:31 +0430

    late work

--- java/ir/cdesign/contactrate/utilities/WallpaperBoy.java
+++ java/ir/cdesign/contactrate/utilities/WallpaperBoy.java
@@ -7,15 +7,6 @@ import android.view.View;
  * Created by amin pc on 06/09/2016.
  */
 public class WallpaperBoy {
-    int imageId, bgId;
-    View view;
 
-    public void setWallpaper(View view, int imageId) {
-        this.view = view;
-        this.imageId = imageId;
-
-        view.setBackgroundResource(imageId);
-
-    }
 
 }
