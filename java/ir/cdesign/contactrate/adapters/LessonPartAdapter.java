fced46b65c0679a35af07a0a2a6d5927647adb82
Amin Ahrabian <aahrabian@gmail.com>
2016-09-19 14:22:22 +0430

    lesson part

---
+++ java/ir/cdesign/contactrate/adapters/LessonPartAdapter.java
@@ -0,0 +1,7 @@
+package ir.cdesign.contactrate.adapters;
+
+/**
+ * Created by amin pc on 19/09/2016.
+ */
+public class LessonPartAdapter {
+}
---
+++ java/ir/cdesign/contactrate/lessons/LessonPartActivity.java
@@ -0,0 +1,15 @@
+package ir.cdesign.contactrate.lessons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.cdesign.contactrate.R;
+
+public class LessonPartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson_part_activity);
+    }
+}
