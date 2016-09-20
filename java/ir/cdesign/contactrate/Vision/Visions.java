88e86da2cee61cb08a4c744a8b0fea7bffca0a11
Amin Ahrabian <aahrabian@gmail.com>
2016-09-20 15:11:15 +0430

    branch bde

--- java/ir/cdesign/contactrate/Vision/Visions.java
+++ java/ir/cdesign/contactrate/Vision/Visions.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
--- java/ir/cdesign/contactrate/demoedition/VisionDemo.java
+++ java/ir/cdesign/contactrate/demoedition/VisionDemo.java
@@ -2,6 +2,7 @@ package ir.cdesign.contactrate.demoedition;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import ir.cdesign.contactrate.R;
 
@@ -10,6 +11,7 @@ public class VisionDemo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.demo_vision_activity);
 
 
