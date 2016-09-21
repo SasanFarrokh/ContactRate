0cd5de29bba7763caf90274ed5c5a52703f6f7f8
Amin Ahrabian <aahrabian@gmail.com>
2016-09-21 09:11:35 +0330

    making the font library work

---
+++ java/ir/cdesign/contactrate/utilities/Application.java
@@ -0,0 +1,21 @@
+package ir.cdesign.contactrate.utilities;
+
+import ir.cdesign.contactrate.R;
+import ir.cdesign.contactrate.caligraphy.CalligraphyConfig;
+
+/**
+ * Created by amin pc on 21/09/2016.
+ */
+public class Application extends android.app.Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("iransans.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+
+}
